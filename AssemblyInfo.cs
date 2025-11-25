7ddd8c70c944ce99e294497f79f435ac1721d8a0
Alexey-Misulev <a.misulev@carx-tech.com>
2025-11-25 22:33:23 +0300

    added AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("RenderCore.Editor")]
+
+namespace RenderCore
+{
+	public static class AssemblyInfo
+	{
+		internal const string AssemblyVersion = "2.7.0";
+
+		public static Version version => new Version(AssemblyVersion);
+	}
+}
