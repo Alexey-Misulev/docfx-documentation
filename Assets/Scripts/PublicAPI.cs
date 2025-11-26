2594d0548b02f6384ca1f7954656f195d3a4660a
Alexey Misulev <a.misulev@carx-tech.com>
2025-11-26 10:16:55 +0300

    8

---
+++ Assets/Scripts/PublicAPI.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Пример публичного API для документации.
+/// </summary>
+public static class PublicAPI
+{
+    /// <summary>
+    /// Складывает два числа.
+    /// </summary>
+    public static int Add(int a, int b) => a + b;
+}
