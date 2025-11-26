e4a8283778a54e46c98336559f8f6c2186fdc748
Alexey Misulev <a.misulev@carx-tech.com>
2025-11-26 11:45:14 +0300

    4

--- ExampleClass.cs
+++ ExampleClass.cs
@@ -1,13 +1,16 @@
-/// <summary>
-/// Пример класса для документации.
-/// </summary>
-public class ExampleClass
+namespace ExampleNamespace
 {
     /// <summary>
-    /// Складывает два числа.
+    /// Пример класса для документации.
     /// </summary>
-    /// <param name="a">Первое число.</param>
-    /// <param name="b">Второе число.</param>
-    /// <returns>Сумма a и b.</returns>
-    public static int Add(int a, int b) => a + b;
+    public class ExampleClass
+    {
+        /// <summary>
+        /// Складывает два числа.
+        /// </summary>
+        /// <param name="a">Первое число.</param>
+        /// <param name="b">Второе число.</param>
+        /// <returns>Сумма a и b.</returns>
+        public static int Add(int a, int b) => a + b;
+    }
 }
